03545001c685b6c087420e4c9c7f128e80e2230b
Nzparra <nzparra@gmail.com>
2020-07-15 15:32:36 -0500

    [ADD]Matrix operations

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" addition, subtraction, multiplication, and division """
+
+
+def np_elementwise(mat1, mat2):
+    """ addition, subtraction, multiplication, and division """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
