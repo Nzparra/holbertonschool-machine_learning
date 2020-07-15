a03d6f0115d764e6730ba3a36875ab31f72747b3
Nzparra <nzparra@gmail.com>
2020-07-15 15:39:15 -0500

    [FIX]Alias of Numpy

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -2,9 +2,9 @@
 """ concatenates two matrices along a specific axis """
 
 
-import numpy
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """ concatenates two matrices along a specific axis"""
-    return numpy.concatenate((mat1, mat2), axis=axis)
+    return np.concatenate((mat1, mat2), axis=axis)
