4c70bc8148439cf4b687e78b16ed0d7b2f6a8fbd
Nzparra <nzparra@gmail.com>
2020-07-24 01:31:17 -0500

    [FIX]0 Value

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -7,5 +7,5 @@ def summation_i_squared(n):
     Return the integer value of the sum
     If n is not a valid number, return None
     """
-    return sum(list(map(lambda x: x*x, list(range(1, n+1))))) \
-        if (n is not None or n >= 0) else None
+    return sum(list(map(lambda x: x**2, list(range(1, n+1))))) \
+        if (n is not None or n > 0) else None
