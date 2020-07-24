c8644ef1c11b63c0defd63b203a49a4438853f8a
Nzparra <nzparra@gmail.com>
2020-07-24 01:37:32 -0500

    [FIX]0 Value

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" Calculares summation_i_squared """
 
 
 def summation_i_squared(n):
@@ -8,4 +9,4 @@ def summation_i_squared(n):
     If n is not a valid number, return None
     """
     return sum(list(map(lambda x: x**2, list(range(1, n+1))))) \
-        if (n is not None or n > 0) else None
+        if (n > 0) else None
