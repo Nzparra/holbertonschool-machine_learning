0ff1aefa7ef0b77c78853238f9c0159ccfc61fda
Nzparra <nzparra@gmail.com>
2020-08-08 22:51:30 -0500

    [ADD]Forward Propagation

---
+++ supervised_learning/0x00-binary_classification/2-neuron.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" Neuron that defines a single neuron performing binary classification """
+
+import numpy as np
+
+
+class Neuron():
+    """
+        W: The weights vector for the neuron. Upon instantiation,
+        it should be initialized using a random normal distribution.
+        b: The bias for the neuron. Upon instantiation,
+        it should be initialized to 0.
+        A: The activated output of the neuron (prediction).
+        Upon instantiation, it should be initialized to 0.
+    """
+    def __init__(self, nx):
+        """
+            nx is the number of input features to the neuron
+            If nx is not an integer,
+            raise a TypeError with the exception: nx must be an integer
+            If nx is less than 1, raise a ValueError with the exception:
+            nx must be a positive integer
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx <= 0:
+            raise ValueError("nx must be a positive integer")
+        self.__b = 0
+        self.__A = 0
+        self.__W = np.array([np.random.randn(nx)])
+
+    @property
+    def W(self):
+        """  The weights vector for the neuron """
+        return self.__W
+
+    @property
+    def b(self):
+        """ The bias for the neuron """
+        return self.__b
+
+    @property
+    def A(self):
+        """ The bias for the neuron """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Calculates the forward propagation of the neuron """
+        Z = np.matmul(self.__W, X)
+        self.__A = 1 / (1 + np.exp(Z * -1))
+        return self.__A
