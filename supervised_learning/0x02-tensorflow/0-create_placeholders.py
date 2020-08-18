71841db46d6c410073bf74d1b86a636068781f01
Nzparra <nzparra@gmail.com>
2020-08-17 21:11:39 -0500

    [ADD]Placeholders

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" returns two placeholders, x and y, for the neural network """
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def create_placeholders(nx, classes):
+    """
+        nx: the number of feature columns in our data
+        classes: the number of classes in our classifier
+        Returns: placeholders named x and y, respectively
+    """
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+    return (x, y)
