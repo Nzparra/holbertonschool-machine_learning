8c44b7dce0c0c1c6976c83025876b6a642455dcb
Nzparra <nzparra@gmail.com>
2020-08-23 11:58:07 -0500

    [FIX]mini

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """  trains a loaded neural network model using mini-batch gradient descent """
 
-# import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 shuffle = __import__('2-shuffle_data').shuffle_data
 
 
@@ -41,3 +39,19 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
             print('\tTraining Accuracy: {}'.format(T_acc))
             print('\tValidation Cost: {}'.format(V_cost))
             print('\tValidation Accuracy: {}'.format(V_acc))
+            if i < epochs:
+                X, Y = shuffle(X_train, Y_train)
+                for j in range(iter_):
+                    start = j * batch_size
+                    end = (j + 1) * batch_size
+                    if end > X_train.shape[0]:
+                        end = X_train.shape[0]
+                    temp = {x: X[start:end], y: Y[start:end]}
+                    ss.run(train_op, feed_dict=temp)
+                    if j != 0 and (j + 1) % 100 == 0:
+                        print("\tStep {}:".format(j + 1))
+                        print("\t\tCost: {}".format(ss.run(loss,
+                                                           feed_dict=temp)))
+                        print("\t\tAccuracy: {}".format(
+                            ss.run(acc, feed_dict=temp)))
+        return save.save(ss, save_path)
