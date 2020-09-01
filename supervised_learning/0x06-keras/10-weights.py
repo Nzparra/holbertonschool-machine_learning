729810a381283a749d575cd309a05f86cc4e56b5
Nzparra <nzparra@gmail.com>
2020-09-01 14:24:19 -0500

    [ADD]Save and Loaf

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" saves an entire model """
+
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """
+        network is the model to save
+        filename is the path of the file that the model should be saved to
+        Returns: None
+    """
+    K.models.save_weights(filepath=filename, save_format=save_format)
+    return None
+
+
+def load_model(filename):
+    """
+        network is the model to which the weights should be loaded
+        filename path of the file that the weights should be loaded from
+        Returns: None
+    """
+    K.models.save_weights(filepath=filename)
+    return None
