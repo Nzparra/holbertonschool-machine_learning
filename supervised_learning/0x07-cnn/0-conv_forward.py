d812e0a90170be05f4b8c347f9b29d3d10721ec9
Nzparra <nzparra@gmail.com>
2020-09-10 23:51:55 -0500

    [FIX]Loop range and varaibe name

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -11,7 +11,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         Returns: the output of the convolutional layer
     """
     sh, sw = stride
-    kh, kw, c_prev, c_aft = W.shape
+    kh, kw, c_prev, c_new = W.shape
     m, h_prev, w_prev, c_prev = A_prev.shape
     if padding != "same":
         ph, pw = (0, 0)
@@ -21,10 +21,10 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     new = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
     conv_h = int(((h_prev + (2 * ph) - kh) / sh) + 1)
     conv_w = int(((w_prev + (2 * pw) - kw) / sw) + 1)
-    conv = np.zeros((m, conv_h, conv_w, c_aft))
+    conv = np.zeros((m, conv_h, conv_w, c_new))
     for i in range(conv_h):
         for j in range(conv_w):
-            for k in range(c_prev):
+            for k in range(c_new):
                 st = i * sh
                 end = (i * sh) + kh
                 stw = j * sw
