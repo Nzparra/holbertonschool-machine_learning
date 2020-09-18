6c4daedc5e5826c95d313b56c768d639be2827ad
Nzparra <nzparra@gmail.com>
2020-09-18 15:49:24 -0500

    [UPD]Correct file

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -2,39 +2,41 @@
 """ builds an identity block as described"""
 
 import tensorflow.keras as K
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
 
 
-def projection_block(A_prev, filters, s=2):
+def resnet50():
     """
     Returns: the activated output of the identity block
     """
-    F11, F3, F12 = filters
+    X = K.Input(shape=(224, 224, 3))
     lay_init = K.initializers.he_normal()
-    conv_layF11 = K.layers.Conv2D(filters=F11, kernel_size=(1, 1),
-                                  padding='same', strides=(s, s),
-                                  kernel_initializer=lay_init)
-    convF11 = conv_layF11(A_prev)
-    norm_lay1 = K.layers.BatchNormalization(axis=3)
-    norm1 = norm_lay1(convF11)
+    conv1 = K.layers.Conv2D(filters=64, kernel_size=(7, 7),
+                            padding='same', strides=(2, 2),
+                            kernel_initializer=lay_init)(X)
+    norm1 = K.layers.BatchNormalization(axis=3)(conv1)
     X1 = K.layers.Activation('relu')(norm1)
-    conv_layF3 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
-                                 padding='same',
-                                 kernel_initializer=lay_init)
-    convF3 = conv_layF3(X1)
-    norm_lay2 = K.layers.BatchNormalization(axis=3)
-    norm2 = norm_lay2(convF3)
-    X2 = K.layers.Activation('relu')(norm2)
-    conv_layF12 = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
-                                  padding='same',
-                                  kernel_initializer=lay_init)
-    convF12 = conv_layF12(X2)
-    norm_lay3 = K.layers.BatchNormalization(axis=3)
-    norm3 = norm_lay3(convF12)
-    shortcut_lay = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
-                                   padding='same', strides=(s, s),
-                                   kernel_initializer=lay_init)
-    shortcut = shortcut_lay(A_prev)
-    norm_layshort = K.layers.BatchNormalization(axis=3)
-    norm_short = norm_layshort(shortcut)
-    result = K.layers.Add()([norm3, norm_short])
-    return K.layers.Activation('relu')(result)
+    mxpool1 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
+                                    padding="same")(X1)
+    pro_block0 = projection_block(mxpool1, [64, 64, 256], 1)
+    ide_block1 = identity_block(pro_block0, [64, 64, 256])
+    ide_block2 = identity_block(ide_block1, [64, 64, 256])
+    pro_block1 = projection_block(ide_block2, [128, 128, 512])
+    ide_block3 = identity_block(pro_block1, [128, 128, 512])
+    ide_block4 = identity_block(ide_block3, [128, 128, 512])
+    ide_block5 = identity_block(ide_block4, [128, 128, 512])
+    pro_block2 = projection_block(ide_block5, [256, 256, 1024])
+    ide_block6 = identity_block(pro_block2, [256, 256, 1024])
+    ide_block7 = identity_block(ide_block6, [256, 256, 1024])
+    ide_block8 = identity_block(ide_block7, [256, 256, 1024])
+    ide_block9 = identity_block(ide_block8, [256, 256, 1024])
+    ide_block10 = identity_block(ide_block9, [256, 256, 1024])
+    pro_block3 = projection_block(ide_block10, [512, 512, 2048])
+    ide_block11 = identity_block(pro_block3, [512, 512, 2048])
+    ide_block12 = identity_block(ide_block11, [512, 512, 2048])
+    avgpool = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                        strides=(1, 1))(ide_block12)
+    FC = K.layers.Dense(units=1000, activation='softmax',
+                        kernel_initializer=lay_init)(avgpool)
+    return K.models.Model(inputs=X, outputs=FC)
