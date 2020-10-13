d3041d0f12f83755fb5dfbe4ae95924984a4557f
Nzparra <nzparra@gmail.com>
2020-10-13 01:48:37 -0500

    [ADD]Train and evaluete

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python3
+""" Restore save trained DNN """
+
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    Returns: the networks prediction, accuracy, and loss,
+             respectively
+    """
+    with tf.Session() as ses:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(ses, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        loss = tf.get_collection("loss")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        out = ses.run(y_pred, feed_dict={x: X, y: Y})
+        acc = ses.run(accuracy, feed_dict={x: X, y: Y})
+        cost = ses.run(loss, feed_dict={x: X, y: Y})
+    return (out, acc, cost)
