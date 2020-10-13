f47cec82ba07e1fe8f331266df45088c59e6c08d
Nzparra <nzparra@gmail.com>
2020-10-13 01:50:50 -0500

    [ADD]evaluate fiz

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -9,15 +9,15 @@ def evaluate(X, Y, save_path):
     Returns: the networks prediction, accuracy, and loss,
              respectively
     """
-    with tf.Session() as ses:
-        saver = tf.train.import_meta_graph(save_path + '.meta')
-        saver.restore(ses, save_path)
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection("y_pred")[0]
-        loss = tf.get_collection("loss")[0]
-        accuracy = tf.get_collection("accuracy")[0]
-        out = ses.run(y_pred, feed_dict={x: X, y: Y})
-        acc = ses.run(accuracy, feed_dict={x: X, y: Y})
-        cost = ses.run(loss, feed_dict={x: X, y: Y})
-    return (out, acc, cost)
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph('{}.meta'.format(save_path))
+        saver.restore(sess, '{}'.format(save_path))
+        x, *_ = tf.get_collection('x')
+        y, *_ = tf.get_collection('y')
+        y_pred, *_ = tf.get_collection('y_pred')
+        loss, *_ = tf.get_collection('loss')
+        accuracy, *_ = tf.get_collection('accuracy')
+        net_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        net_loss = sess.run(loss, feed_dict={x: X, y: Y})
+        net_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+    return net_pred, net_accuracy, net_loss
