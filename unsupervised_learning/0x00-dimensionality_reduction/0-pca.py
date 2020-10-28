3ad9a746cc2599320feb12b4c07256897ad4432e
Nzparra <nzparra@gmail.com>
2020-10-28 14:10:30 -0500

    [ADD]W

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
-""" Getting weights of PCA """
+""" PCA """
 
 import numpy as np
 
 
 def pca(X, var=0.95):
+    """ Function that performs PCA on a dataset
+    Args:
+        X - is a numpy.ndarray of shape (n, d) where:
+            n is the number of data points
+            d is the number of dimensions in each point
+            all dimensions have a mean of 0 across all data points
+        var - is the fraction of the variance that the PCA transformation
+            should maintain
+    Returns:
+        The weights matrix, W, that maintains var fraction of X‘s original
+        variance
     """
-    Returns: the weights matrix, W, that maintains var fraction
-    of X‘s original variance
-    """
-    U, S, VT = np.linalg.svd(X)
-    t_variance = np.cumsum(S) / np.sum(S)
-    r = np.argwhere(t_variance >= var)[0, 0]
-    W = VT[:r + 1].T
+    u, s, vh = np.linalg.svd(X)
+    total_variance = np.cumsum(s) / np.sum(s)
+    r = np.argwhere(total_variance >= var)[0, 0]
+    W = vh[:r + 1].T
     return W
