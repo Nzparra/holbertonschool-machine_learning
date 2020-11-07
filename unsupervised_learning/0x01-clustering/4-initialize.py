50a01565016789c322b2716f02d076a3a2c2216b
Nzparra <nzparra@gmail.com>
2020-11-06 19:19:48 -0500

    [ADD]Intilize

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
-""" Initialize """
+""" initialize  """
 
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
 
 
-def pdf(X, m, S):
+def initialize(X, k):
     """
-        Returns: P, or None on failure
+        Returns: pi, m, S, or None, None, None on failure
     """
     if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None
-    if type(m) is not np.ndarray or len(m.shape) != 1:
-        return None
-    if type(S) is not np.ndarray or len(S.shape) != 2:
-        return None
-    if X.shape[1] != S.shape[1] or S.shape[0] != S.shape[1]:
-        return None
-    if X.shape[1] != m.shape[0]:
-        return None
-    _, d = X.shape
-    det = np.linalg.det(S)
-    first = np.matmul((X - m), np.linalg.inv(S))
-    second = np.sum(first * (X - m), axis=1)
-    num = np.exp(second / -2)
-    den = np.sqrt(det) * ((2 * np.pi) ** (d/2))
-    pdf = num / den
-    pdf = np.where(pdf < 1e-300, 1e-300, pdf)
-    return pdf
+        return (None, None, None)
+    if type(k) is not int or k <= 0:
+        return (None, None, None)
+    n, d = X.shape
+    m, _ = kmeans(X, k)
+    pi = np.ones(k) / k
+    ident = np.identity(d).reshape(-1)
+    S = (np.tile(ident, k)).reshape(k, d, d)
+    return (pi, m, S)
