8b6e33d58139512fdf38379d3d46b5c519baf741
Nzparra <nzparra@gmail.com>
2021-03-15 18:01:11 -0500

    update 2-USER_LOCATION

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
-""" Consuming API """
-import requests
+"""
+Printing the location of a specific user
+"""
 import sys
+import requests
 import time
 
+
 if __name__ == '__main__':
+
     url = sys.argv[1]
-    headers = {'Accept': 'application/vnd.github.v3+json'}
-    data = requests.get(url, headers=headers)
-    if data.status_code == 200:
-        print(data.json()['location'])
-    if data.status_code == 404:
+    payload = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url, params=payload)
+    # Status: 403 Forbidden
+    if r.status_code == 403:
+        limit = r.headers["X-Ratelimit-Reset"]
+        x = (int(limit) - int(time.time())) / 60
+        print("Reset in {} min".format(int(x)))
+    # Status: 200 OK
+    if r.status_code == 200:
+        location = r.json()["location"]
+        print(location)
+    # Status: 404 Not Found
+    if r.status_code == 404:
         print("Not found")
-    if data.status_code == 403:
-        limit_time = int(data.headers['X-Ratelimit-Reset'])
-        now = int(time.time())
-        result = int((limit_time - now) / 60)
-        print("Reset in {} min".format(result))
